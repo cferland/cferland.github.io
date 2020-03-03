ada77232488a7c59c198aebace54da1dc6d571aa
Corey Ferland <55623949+cferland@users.noreply.github.com>
2020-03-03 11:43:39 -0500

    updated mobile CSS

---
+++ precache-manifest.fb64d92e8719f465aec85626a80d6deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbf2c212da98d9d3d0c04ab9dad01cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a53090f300d9dac67043",
+    "url": "/static/css/main.ff312f4f.chunk.css"
+  },
+  {
+    "revision": "80b908f5952bd2e56bff",
+    "url": "/static/js/2.5b9cba86.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.5b9cba86.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a53090f300d9dac67043",
+    "url": "/static/js/main.663e493c.chunk.js"
+  },
+  {
+    "revision": "00ccf1beb4d435cfbe60",
+    "url": "/static/js/runtime-main.c7cc6e23.js"
+  }
+]);
